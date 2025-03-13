986d295c6973479414e048b6d87b3e7715a89bf2
David Andino <david.andinod@gmail.com>
2025-03-13 12:54:16 +0100

    Fixing error of type definition in the ValkeyBackup

--- tools/cmd.sh
+++ tools/cmd.sh
@@ -6,14 +6,14 @@ operator-sdk create api --group valkey --version v1alpha1 --kind ValkeyBackup
 
 # After file customization and edition, previously login: docker login quay.io
 # After creation remember to make the repo public
-make docker-build docker-push IMG=quay.io/$USERNAME/valkey-operator:v0.0.3
+make docker-build docker-push IMG=quay.io/$USERNAME/valkey-operator:v0.0.4
 
 
 # To deploy the operator
-make deploy IMG=quay.io/$USERNAME/valkey-operator:v0.0.1
+make deploy IMG=quay.io/$USERNAME/valkey-operator:v0.0.4
 
 # After deployment: kubectl get all -n valkey-operator-system
 # To change the name of the namespace: config/default/kustomization.yaml
 
 # To undeploy
-make undeploy IMG=quay.io/$USERNAME/valkey-operator:v0.0.1
+make undeploy IMG=quay.io/$USERNAME/valkey-operator:v0.0.4
