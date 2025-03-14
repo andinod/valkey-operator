5eed88950b215593d39af5e6abb20be5a24e6696
David Andino <david.andinod@gmail.com>
2025-03-14 11:07:24 +0100

    Adding new documentation as well as the new version of the valkeybackup chart with the Valkey Type enabled

--- tools/cmd.sh
+++ tools/cmd.sh
@@ -21,3 +21,15 @@ make undeploy IMG=quay.io/$USERNAME/valkey-operator:v0.0.4
 
 # To get latest versions from helm
 helm search repo andinod/valkey --versions
+
+
+# To modify the current image:
+- Make changes
+- Login to quay.io : docker login quay.io
+- Set the USERNAME variable to your quay.io valid username
+- Build the new image: make docker-build docker-push IMG=quay.io/$USERNAME/valkey-operator:v0.0.12
+- Update the helm chart: make helm
+- Update the Chart version and Values if altered in charts/valkey-operator, check if the image version corresponds with the one recently build
+- Check the changes: git status
+- Commit the changes and push to github. 
+- Wait until the new version is generated and apply the new version of the operator through helm
